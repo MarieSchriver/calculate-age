69b21ff6905fd4c7a426363fc0815692ad7f6bd5
MarieSchriver <eaa24masb@students.eaaa.dk>
2025-01-27 09:23:59 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+let birthYear = prompt("Please enter your birth year:");
+
+let currentYear = new Date().getFullYear();
+let age = currentYear - parseInt(birthYear);
+
+console.log("Your age is: " + age);
